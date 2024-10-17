36e42378428d49fa48bc3c83b9d22d08b579acec
sahilnamdeo2002 <sahilnamdeo12@gmail.com>
2024-10-17 17:08:09 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Home.css";
 // import image from "../images/gym.jpg";
-
+// import dragon1 from "./3dslider/dragon_1.jpg";
 
 
 const Home = () => {
@@ -52,8 +52,36 @@ const Home = () => {
 
 
             <div id="features">
-           
+                <div className="banner">
+                    <div className="slider" style={{ '--quantity': 10 }}>
+                        <div className="item" style={{ '--position': 1 }}><img src="./3dslider/dragon_1.jpg" alt="Dragon 1" /></div>
+                        <div className="item" style={{ '--position': 2 }}><img src="/3dslider/dragon_2.jpg" alt="Dragon 2" /></div>
+                        <div className="item" style={{ '--position': 3 }}><img src="/3dslider/dragon_3.jpg" alt="Dragon 3" /></div>
+                        <div className="item" style={{ '--position': 4 }}><img src="/3dslider/dragon_4.jpg" alt="Dragon 4" /></div>
+                        <div className="item" style={{ '--position': 5 }}><img src="/3dslider/dragon_5.jpg" alt="Dragon 5" /></div>
+                        <div className="item" style={{ '--position': 6 }}><img src="/3dslider/dragon_6.jpg" alt="Dragon 6" /></div>
+                        <div className="item" style={{ '--position': 7 }}><img src="/3dslider/dragon_7.jpg" alt="Dragon 7" /></div>
+                        <div className="item" style={{ '--position': 8 }}><img src="/3dslider/dragon_8.jpg" alt="Dragon 8" /></div>
+                        <div className="item" style={{ '--position': 9 }}><img src="/3dslider/dragon_9.jpg" alt="Dragon 9" /></div>
+                        <div className="item" style={{ '--position': 10 }}><img src="/3dslider/dragon_10.jpg" alt="Dragon 10" /></div>
+                    </div>
+                    <div className="content">
+                        <h1 data-content="CSS ONLY">CSS ONLY</h1>
+                        <div className="author">
+                            <h2>LUN DEV</h2>
+                            <p><b>Web Design</b></p>
+                            <p>Subscribe to the channel to watch many interesting videos</p>
+                        </div>
+                        <div className="model"></div>
+                    </div>
+                </div>
             </div>
+
+
+
+
+
+
         </div>
 
     )
